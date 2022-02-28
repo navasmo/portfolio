c3002521aaefb46da29cdd72eeaf7cb342f5666c
Navas Mohamed <vhnavasse@gmail.com>
2022-02-28 23:02:46 +0000

    image fix

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -19,7 +19,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src=".assets/mnk.png" alt="" />
+          <img src="assets/mnk.png" alt="" />
         </div>
       </div>
       <div className="right">
