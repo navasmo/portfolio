4ad3da1ec4cc422b0b2da9e888aa8fa690497f70
Navas Mohamed <vhnavasse@gmail.com>
2022-03-01 01:32:41 +0000

    contact form connected to email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./contact.scss";
+import emailjs from "emailjs-com";
 
-export default function Contact() {
-  const [message, setMessage] = useState(false);
+const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMessage(true);
+    emailjs
+      .sendForm(
+        "service_ltyww77",
+        "template_6f6y8an",
+        formRef.current,
+        "73Ul5C0HyqmNs4dYg"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
   return (
     <div className="contact" id="contact">
@@ -15,13 +32,17 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
-          <button type="submit">Send</button>
-          {message && <span>Thank you for reaching out, I'll get back to ASAP :)</span>}
-        </form>
+        <form ref={formRef} onSubmit={handleSubmit}>
+            <input  type="text" placeholder="Name" name="user_name" />
+            <input  type="text" placeholder="Subject" name="user_subject" />
+            <input  type="text" placeholder="Email" name="user_email" />
+            <textarea  rows="5" placeholder="Message" name="message" />
+            <button>Send</button>
+             {done && <span>Thank you for reaching out, I'll get back to ASAP :)</span>}
+          </form>
       </div>
     </div>
   );
 }
+
+export default Contact;
