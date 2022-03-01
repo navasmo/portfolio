b35c6cbfa0ab57801998fd3d614536dbc40baf66
Navas Mohamed <vhnavasse@gmail.com>
2022-03-01 13:51:37 +0000

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,9 @@
-import { useRef, useState } from "react";
+import {  useRef, useState } from "react";
+import { Phone, Mail, GitHub, LinkedIn, CloudDownload } from "@material-ui/icons";
 import "./contact.scss";
 import emailjs from "emailjs-com";
+import Button from '@material-ui/core/Button';
+
 
 const Contact = () => {
   const formRef = useRef();
@@ -19,6 +22,7 @@ const Contact = () => {
         (result) => {
           console.log(result.text);
           setDone(true)
+  
         },
         (error) => {
           console.log(error.text);
@@ -26,9 +30,30 @@ const Contact = () => {
       );
   };
   return (
+    
     <div className="contact" id="contact">
       <div className="left">
         <img src="assets/shake.svg" alt="" />
+        <div className="itemContainer">
+            <Phone className="icon" />
+            <span>+44 7429006011</span>
+          </div>
+          <Button className="itemContainer" style={{textTransform: 'none'}} href="mailto:vhnavasse@gmail.com">
+            <Mail className="icon" />
+            <span>vhnavasse@gmail.com</span>
+          </Button>
+          <Button className="itemContainer" style={{textTransform: 'none'}} href="https://github.com/navasmo" target="_blank">
+            <GitHub className="icon" />
+            <span>navasmo</span>
+          </Button>
+          <Button className="itemContainer"  style={{textTransform: 'none'}} href="https://www.linkedin.com/in/navasmo/" target="_blank">
+            <LinkedIn className="icon" />
+            <span>navasmo</span>
+          </Button>
+          <Button className="itemContainer" style={{textTransform: 'none'}} href="https://docs.google.com/document/d/1sg4oUc7RyvzX2vc4rHxzohqS61IWs9ts/edit?usp=sharing&ouid=107024906914517971505&rtpof=true&sd=true">
+            <CloudDownload className="icon" />
+            <span>resume</span>
+          </Button>
       </div>
       <div className="right">
         <h2>Contact.</h2>
@@ -37,9 +62,10 @@ const Contact = () => {
             <input  type="text" placeholder="Subject" name="user_subject" />
             <input  type="text" placeholder="Email" name="user_email" />
             <textarea  rows="5" placeholder="Message" name="message" />
-            <button>Send</button>
+            <button >Send</button >
              {done && <span>Thank you for reaching out, I'll get back to ASAP :)</span>}
-          </form>
+             
+          </form >
       </div>
     </div>
   );
